b4b7ab9ce2025794a6f3d27ddc3c51c0c4641d35
Anne Heij <anneheij82@gmail.com>
2024-05-17 09:19:49 +0200

    solved tiny mistake in comment

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     path: '/mediator',
     name: 'mediator',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (mediator.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "mediatorview" */ '@/views/MediatorView.vue')
   },
