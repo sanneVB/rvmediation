e805cc5fcf9ff36dca6fa6a7ef855bdec9867d51
Sanne van Boheemen <114480939+sanneVB@users.noreply.github.com>
2024-05-22 19:51:51 +0200

    Add hero image and nav button

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Header</h1>
+  <button @click="" id="nav-toggle" >
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+  </button>
   <navigation-menu/>
 
 </template>
@@ -14,3 +18,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+  #nav-toggle {
+    border: 2px solid #FFFF;
+    border-radius: 5px;
+    background-color: transparent;
+    padding: 4px 8px;
+    z-index: 2;
+  }
+
+  .bar {
+    width: 24px;
+    height: 2px;
+    background-color: #FFFFFF;
+    margin: 6px 0;
+    transition: 0.2s;
+    border-radius: 4px;
+  }
+
+  
+
+</style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Landing Page</h1>
+  <div class="homeView">
+
+    <h1>Rob</h1>
+    <h1>Verbeek</h1>
+  </div>
+
 </template>
+
+<script>
+export default{
+}
+</script>
+
+<style scoped>
+  .homeView {
+    background-image: url('../assets/hero.jpg');
+    background-size:cover;
+    background-position:50% 10%;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+
+
+</style>
