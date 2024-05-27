746212ee03535bf5b6fcd54fc3bf899bd51d7aa1
Sanne van Boheemen <114480939+sanneVB@users.noreply.github.com>
2024-05-27 14:50:35 +0200

    Remove temporary styling

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -15,43 +15,4 @@ export default {
 </script>
 
 <style scoped>
-
-  .button-nav {
-    border: 2px solid #FFFF;
-    border-radius: 5px;
-    background-color: transparent;
-    padding: 4px 8px;
-    z-index: 2;
-    position: sticky;
-  }
-
-  .button-nav__bar {
-    width: 24px;
-    height: 2px;
-    background-color: #FFFFFF;
-    margin: 6px 0;
-    transition: 0.2s;
-    border-radius: 4px;
-  }
-
-  
-
 </style>
-
-<!--<template>
-  <header :class="{ sticky: isSticky }">
-    <div class="logo">Mijn Logo</div>
-    <nav-component/>
-  </header>
-</template>
-
-<script>
-export default {
-  props: {
-    isSticky: {
-      type: Boolean,
-      default: false
-    }
-  }
-};
-</script>-->
