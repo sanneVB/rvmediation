ccfcff83edfc35d65827b87424dac435c9733d53
Sanne van Boheemen <114480939+sanneVB@users.noreply.github.com>
2024-05-27 14:48:52 +0200

    Remove duplicate images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,28 +23,28 @@
 </template>
 
 <script>
-import HeaderContent from "@/components/HeaderContent.vue";
-import LandingPage from "@/components/LandingPage.vue";
-import IntroductionPage from "@/components/IntroductionPage.vue";
-import EmployeePage from "@/components/EmployeePage.vue";
-import EmployerPage from "@/components/EmployerPage.vue";
-import AboutMePage from "@/components/AboutMePage.vue";
-import TestimonialsPage from "@/components/TestimonialsPage.vue";
-import FooterContent from "@/components/FooterContent.vue";
+  import HeaderContent from "@/components/HeaderContent.vue";
+  import LandingPage from "@/components/LandingPage.vue";
+  import IntroductionPage from "@/components/IntroductionPage.vue";
+  import EmployeePage from "@/components/EmployeePage.vue";
+  import EmployerPage from "@/components/EmployerPage.vue";
+  import AboutMePage from "@/components/AboutMePage.vue";
+  import TestimonialsPage from "@/components/TestimonialsPage.vue";
+  import FooterContent from "@/components/FooterContent.vue";
 
-export default {
-name: 'HomeView',
-components: {
-  HeaderContent,
-  LandingPage,
-  IntroductionPage,
-  EmployeePage,
-  EmployerPage,
-  AboutMePage,
-  TestimonialsPage,
-  FooterContent
-}
-}
+  export default {
+    name: 'HomeView',
+    components: {
+      HeaderContent,
+      LandingPage,
+      IntroductionPage,
+      EmployeePage,
+      EmployerPage,
+      AboutMePage,
+      TestimonialsPage,
+      FooterContent
+    }
+  }
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -58,17 +58,17 @@ components: {
 
 <style>
 
-html {
-overflow-x: hidden;
-scroll-snap-type: y mandatory;
-height: 100vh;
-overflow-y: scroll;
-}
+  html {
+    overflow-x: hidden;
+    scroll-snap-type: y mandatory;
+    height: 100vh;
+    overflow-y: scroll;
+  }
 
-.container__snapPoint{
-height: 100vh;
-width: 100vw;
-scroll-snap-align: start;
-}
+  .container__snapPoint{
+    height: 100vh;
+    width: 100vw;
+    scroll-snap-align: start;
+  }
 
 </style>
