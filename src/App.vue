74fbcbd3cf1dc628c0ed478a2ac4d1962331240d
Sanne van Boheemen <114480939+sanneVB@users.noreply.github.com>
2024-05-27 16:31:23 +0200

    Add standard font family and several font classes
    Several classes are added to allow for quick application of standard
    weights, slant, et cetera.

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,25 @@
 </template>
 
 <style>
+
+  html {
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-variation-settings: "slnt" 0;
+  }
+
+  .inter--bold {
+    font-weight: 700;
+  }
+
+  .inter--semi-bold {
+    font-weight: 600;
+  }
+
+  .inter--italic {
+    font-variation-settings: "slnt" -10;
+  }
+
 </style>
