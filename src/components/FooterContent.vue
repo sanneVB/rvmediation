eedcc1ce5ff2d4db3ac0433e7d1e7ca1cb004661
Sanne van Boheemen <114480939+sanneVB@users.noreply.github.com>
2024-05-29 11:32:49 +0200

    Remove flex: 1 from footer

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,58 +1,96 @@
 <template>
   <footer>
-    <p>Copyright &copy; {{ currentYear }}, RVmediation | <router-link to="/algemenevoorwaarden">Algemene Voorwaarden</router-link> | <router-link to="/privacystatement">Privacy Statement</router-link></p>
+    <p>
+      Copyright &copy; {{ currentYear }}, RVmediation |
+      <router-link to="/algemenevoorwaarden">Algemene Voorwaarden</router-link>
+      | <router-link to="/privacystatement">Privacy Statement</router-link>
+    </p>
     <div class="logoNMV">
-      <p class="inter inter--semi-bold inter--italic">Lid van</p><img src="../assets/nmv-logo.png" height="32px"/>
+      <p class="inter inter--semi-bold inter--italic">Lid van</p>
+      <img src="../assets/nmv-logo.png" height="32px" />
     </div>
   </footer>
 </template>
 
 <script>
-
-  export default {
-    name: 'FooterContent',
-    data() {
-      return {
-        currentYear: new Date().getFullYear()
-      };
-    }
-    
-  };
+export default {
+  name: "FooterContent",
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
 </script>
 
 <style scoped>
-  footer {
-    background-color: #00AEB8;
-    height: 80px;
-    width: 100vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+footer {
+  background-color: #00aeb8;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  p {
-    color: #FFFFFF;
-    flex: 1;
-    text-align: center;
-  }
+p {
+  color: #ffffff;
+  text-align: center;
+}
 
-  a {
-    color: #ffffff;
-  }
+a {
+  color: #ffffff;
+}
 
-  .logoNMV {
-    position: absolute;
-    right:32px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-  }
+.logoNMV {
+  position: absolute;
+  right: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
 
-  .logoNMV > p {
-    text-align: end;
-    margin: 0;
-    font-size: x-small;
-    margin-bottom: 8px;
-  }
+.logoNMV > p {
+  text-align: end;
+  margin: 0;
+  font-size: x-small;
+  margin-bottom: 8px;
+}
 
+/*footer {
+  background-color: #00aeb8;
+  /~ height: 80px;
+    width: 100vw; ~/
+  display: flex;
+  align-items: center;
+  /~ justify-content: center; ~/
+  padding: 2px 3%;
+}
+
+p {
+  color: #ffffff;
+  /~ flex: 1; ~/
+  margin-right: auto;
+  margin-left: auto;
+  text-align: center;
+  /~ padding-left: 117px; ~/
+}
+
+a {
+  color: #ffffff;
+}
+
+/~ .logoNMV {
+  position: absolute;
+  right: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+} ~/
+
+.logoNMV > p {
+  text-align: end;
+  font-size: x-small;
+  padding-bottom: 8px;
+}*/
 </style>
