e822f898dd68d1c22cd0abf9bb0118e37008f048
Anne Heij <anneheij82@gmail.com>
2024-05-31 10:22:58 +0200

    code the html for the introductionpage

--- src/components/IntroductionPage.vue
+++ src/components/IntroductionPage.vue
@@ -1,5 +1,52 @@
 <template>
-  <h1>Introduction Page</h1>
+  <div class="container">
+    <h2><span class="italic">Uw</span> bemiddelaar voor de logistieke sector</h2>
+    <section class="container__leftcontent">
+      <p>
+        De specialisatie van <span class="bold">RVmediation</span> ligt in de logistieke sector. Door mijn jarenlange ervaring
+        in deze sector zijn mij idverse dingen opgevallen. Er worden hoge verwachtingen aan de werknemers gesteld.
+        Er is sprake van onderliggende conflicten die de sfeer bepalen op de werkvloer, waardoor er veel sprake is
+        van verzuim. Dit komt voort uit een gebrek aan open communicatie.
+      </p>
+      <p>
+        Door mijn persoonlijke kwaliteiten om te zetten in kennis als <span class="layout-mediator">mediator</span> en gedragskundige kan ik het volgende voor uw
+        bedrijf betekenen:
+        <ul>
+          <li>een stabielere bedrijfscommunicatie</li>
+          <li>een stijgend rendement (omzet/winst)</li>
+          <li>behoud van werknemers door tevredenheid</li>
+          <li>een dalend verzuim</li>
+          <li>een prettige werksfeer door open communicatie</li>
+        </ul>
+      </p>
+      <img class="container-img-whitetruck" src="../assets/whitetruck.jpg">
+      <div class="box"></div>
+    </section>
+    <section class="container__rightcontent">
+      <div class="box"></div>
+      <img class="container-img-train" src="../assets/train.jpg">
+      <p>Merkt u op dat er veel verzuim en/of verloop van werknemrs binnen uw bedrijf is?</p>
+      <p>
+        <span class="bold">RVmediation</span> kan u een passende aanpak voor uw bedrijf opzetten. Door
+        met elkaar in gesprek te gaan wordt er ruimte gecreeerd voor een passende oplossing. Dit biedt
+        de basis voor een stabiele en evenwichtige toekomt van uw bedrijf.
+      </p>
+    </section>
+  </div>
 </template>
+
 <script></script>
-<style scoped></style>
+
+<style scoped>
+  
+  .container-img-whitetruck {
+    height: 144px;
+  }
+  
+  .container-img-train {
+    height: 299px;
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+  }
+
+</style>
