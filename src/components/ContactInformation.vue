2ee9caccdd5b95656b4f536b034bf7dab7aaab5f
Sanne van Boheemen <114480939+sanneVB@users.noreply.github.com>
2024-06-03 18:20:13 +0200

    Add first version of contact page makeup

--- src/components/ContactInformation.vue
+++ src/components/ContactInformation.vue
@@ -1,3 +1,66 @@
-<template>Hallo</template>
-<script></script>
-<style scoped></style>
+<template>
+  <div class="contact-information">
+    <h3>RVmediation</h3>
+    <p class="text__contact-information">Lierop, Noord-Brabant</p>
+    <div class="contact-information__phone-email">
+      <p class="text__contact-information">
+        <span class="text__contact-information--bold">Telefoon:</span> 06 481
+        889 92
+      </p>
+      <p class="text__contact-information">
+        <span class="text__contact-information--bold">E-mail:</span
+        ><a href="mailto:info@rvmediation.nl" class="link"
+          >info@rvmediation.nl</a
+        >
+      </p>
+    </div>
+    <img
+      src="../assets/rv-mediation-location-map.png"
+      width="248px"
+      height="186px"
+    />
+    <p class="text__contact-information--small">
+      <span class="text__contact-information--bold">KVK:</span> 758.267.04 •
+      <span class="text__contact-information--bold">BTW:</span>
+      NL.001.891.693.B82
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "ContactInformation",
+};
+</script>
+<style scoped>
+img {
+  object-fit: cover;
+  border-radius: 8px;
+}
+
+.contact-information__phone-email {
+  padding: 25px 0;
+}
+
+.link:link {
+  color: black;
+}
+
+.text__contact-information {
+  font-family: "Inter", sans-serif;
+  font-size: 12px;
+  font-variation-settings: "slnt" 0;
+}
+
+.text__contact-information--small {
+  text-align: center;
+  font-size: 10px;
+}
+
+.text__contact-information--bold {
+  font-weight: 700;
+}
+
+img {
+  margin-bottom: 25px;
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,14 +1,69 @@
 <template>
-  <contact-form />
+  <header-content />
+  <section class="contact-message">
+    <h1>Contact</h1>
+    <p>Heeft u interesse of heeft u vragen? Neem gerust contact op.</p>
+  </section>
+  <div class="grid-container">
+    <div class="grid-container__grid-item grid-container__contact-information">
+      <contact-information />
+      <div class="seperator"></div>
+    </div>
+    <div class="grid-container__grid-item grid-container__form">
+      <contact-form />
+    </div>
+  </div>
 </template>
 <script>
+import HeaderContent from "@/components/HeaderContent.vue";
 import ContactForm from "@/components/ContactForm.vue";
+import ContactInformation from "@/components/ContactInformation.vue";
 
 export default {
   name: "ContactView",
   components: {
+    HeaderContent,
     ContactForm,
+    ContactInformation,
   },
 };
 </script>
-<style></style>
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(128px, 1fr));
+  margin-top: 40px;
+}
+
+.contact-message {
+  text-align: center;
+}
+
+.grid-container__contact-information {
+  padding-left: 17vw;
+  grid-column: span 2;
+  display: flex;
+  justify-content: space-between;
+}
+
+.grid-container__form {
+  padding-right: 15vw;
+  padding-left: 5.57vw;
+  grid-column: span 3;
+}
+
+.contact-message > h1 {
+  margin: 32px 0 40px 0;
+}
+
+.seperator {
+  border-left: 1px solid #7e7e7e;
+  height: 400px;
+}
+
+@media screen and (min-width: 600px) and (max-width: 768px) {
+  .seperator {
+    display: hidden;
+  }
+}
+</style>
