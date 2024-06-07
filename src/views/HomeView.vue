674ec86a1a37c55078c0a7db656bf949286a66fb
olaf.vellinga.bee <olaf.vellinga.bee@outlook.com>
2024-06-07 14:33:57 +0200

    add background colors for all sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -53,7 +53,48 @@ export default {
       this.isVisible = currentVisibility;
     },
   },
+
+  mounted: function() {
+    // ref: https://www.educative.io/answers/how-to-make-a-webpage-that-changes-color-as-you-scroll-down
+    const body = document.querySelector('body');
+    const sections = document.querySelectorAll('section');
+
+    // setting the background color of each of the sections
+    sections.forEach((section, index) => {
+        const bgColors = [
+        '#FDFDFD',
+        '#f2fbfb'  // Same as #00AEB8 with 5% opacity!
+        ];
+
+        section.style.backgroundColor = bgColors[index % bgColors.length];
+    });
+
+    window.addEventListener('scroll', () => {
+    /* calculating the vertical scroll position of the page using 
+        the `window.scrollY` property */
+    const topScroll = window.scrollY;
+    
+    /* calculating the maximum scroll height of the page by subtracting
+        the height of the viewport (innerHeight) from the total height (scrollHeight) */
+    const maximumTopScroll = body.scrollHeight - window.innerHeight;
+    
+    // calculating the scroll fraction
+    const scrollFraction = topScroll / maximumTopScroll;
+    
+    // setting an arbitrary RGB color code as the starting color
+    const [red, green, blue] = [0, 174, 184];  // Same as #00AEB8!
+    
+    // calculating the rgb values based on the scroll fraction
+    const bColor = `rgb(${Math.round(red * scrollFraction)}, 
+    ${Math.round(green * scrollFraction)}, ${Math.round(blue * scrollFraction)})`;
+    
+      /* setting the backgroundColor to bColor for smooth color
+          transition as the user scrolls down the page.*/
+      body.style.backgroundColor = bColor;
+    });
+  }
 };
+
 </script>
 
 <style>
