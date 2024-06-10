6558db6601f6dca378e6ef2a4026c89b882747ef
olaf.vellinga.bee <olaf.vellinga.bee@outlook.com>
2024-06-10 11:45:20 +0200

    correct code spacing and remove blank line

--- src/components/EmployerPage.vue
+++ src/components/EmployerPage.vue
@@ -1,35 +1,34 @@
 <template>
-  
-    <h1>Wat kan ik voor u betekenen als werkgever?</h1>
-    <div class="page-container">
-      <div class="page-container__column-left">
-        <div>
-          <P>Mediation is bedoeld om conflicten tussen werkgever en werknemer op te lossen of tussen twee werknemers. Er kan gekozen worden voor een onafhankelijk bemiddelaar die dit traject begeleidt. Maar beter nog is het voorkomen van een conflict. Dit is een van mijn specialiteiten.</p>
-        </div>
-        <div class="page-container__illustration">
-          <img
-            class="page-container__image"
-            src="../assets/rvmediation-employer-truck-white.jpg"
-            alt="picture of a truck on the road"
-          />
-          <p class="text-box__white-right">“Door Rob zijn grondige werk is het bedrijf weer goed gaan draaien, zoals iedereen het graag ziet.”</p>
-        </div>
+  <h1>Wat kan ik voor u betekenen als werkgever?</h1>
+  <div class="page-container">
+    <div class="page-container__column-left">
+      <div>
+        <P>Mediation is bedoeld om conflicten tussen werkgever en werknemer op te lossen of tussen twee werknemers. Er kan gekozen worden voor een onafhankelijk bemiddelaar die dit traject begeleidt. Maar beter nog is het voorkomen van een conflict. Dit is een van mijn specialiteiten.</p>
       </div>
-      <div class="page-container__column-right">
-        <div class="page-container__illustration">
-          <!-- TODO: Replace red text with new unique text -->
-          <p class="text-box__white-left"><span style="color: red">"Door Rob zijn grondige werk is het bedrijf weer goed gaan draaien, zoals iedereen het graag ziet.”</span></p>
-          <img
-            class="page-container__image"
-            src="../assets/rvmediation-employer-truck-black.jpg"
-            alt="picture of a truck in the city at twilight"
-          />
-        </div>
-        <div>
-          <P>Ik ga door middel van een 'conflictanalyse' kijken waar er in de organisatie potentiële conflicten liggen. Ik ga daarmee deze conflicten vroegtijdig naar een passende oplossing zoeken, waardoor het veel werkbaarder wordt en dat de onderlinge frictie verdwijnt.</p>
-        </div>
+      <div class="page-container__illustration">
+        <img
+          class="page-container__image"
+          src="../assets/rvmediation-employer-truck-white.jpg"
+          alt="picture of a truck on the road"
+        />
+        <p class="text-box__white-right">“Door Rob zijn grondige werk is het bedrijf weer goed gaan draaien, zoals iedereen het graag ziet.”</p>
       </div>
     </div>
+    <div class="page-container__column-right">
+      <div class="page-container__illustration">
+        <!-- TODO: Replace red text with new unique text -->
+        <p class="text-box__white-left"><span style="color: red">"Door Rob zijn grondige werk is het bedrijf weer goed gaan draaien, zoals iedereen het graag ziet.”</span></p>
+        <img
+          class="page-container__image"
+          src="../assets/rvmediation-employer-truck-black.jpg"
+          alt="picture of a truck in the city at twilight"
+        />
+      </div>
+      <div>
+        <P>Ik ga door middel van een 'conflictanalyse' kijken waar er in de organisatie potentiële conflicten liggen. Ik ga daarmee deze conflicten vroegtijdig naar een passende oplossing zoeken, waardoor het veel werkbaarder wordt en dat de onderlinge frictie verdwijnt.</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script></script>
 <style scoped>
