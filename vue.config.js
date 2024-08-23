bcb7faa5366202b9311c4eff9a35ef670b584d75
sanneVB <114480939+sanneVB@users.noreply.github.com>
2024-08-23 15:06:32 +0200

    Undo some gh_pages settings

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  ublicPath: "https://sannevb.github.io/rvmediation/",
   transpileDependencies: true,
   configureWebpack: {
     devtool: "source-map",
